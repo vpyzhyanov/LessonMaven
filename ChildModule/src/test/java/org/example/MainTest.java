66c3a289a7188e5c3a224feec7a77fdf3bbf23ff
vpyzhyanov <lynxviach@gmail.com>
2023-03-21 00:09:50 +0500

    Initial commit

---
+++ ChildModule/src/test/java/org/example/MainTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testMain() {
+        Assertions.assertTrue(true);
+    }
+}
